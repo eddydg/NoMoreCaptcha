e45207865cccb26be0fab022b6d7ce6743528be9
Eddydg <diablo22007@gmail.com>
2013-10-12 13:00:15 +0200

    Added function image2matrix

--- src/image_tools.ml
+++ src/image_tools.ml
@@ -9,3 +9,15 @@ let print_image img surf =
   let pic = Sdlvideo.display_format img in
   Sdlvideo.blit_surface pic surf ();
   Sdlvideo.flip surf
+
+(* Convert image to matrix  *)
+let image2matrix image =
+  let (w, h) = get_dim image in
+  let matrix = Array.make_matrix w h (255,255,255) in
+  for y = 0 to h-1 do
+    for x = 0 to w-1 do
+      matrix.(x).(y) <- Sdlvideo.get_pixel_color image x y
+    done;
+  done;
+  matrix;;
+
