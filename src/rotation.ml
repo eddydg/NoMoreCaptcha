f44d038a7365f7ceaa800213d56f7a5453bb994b
Eddydg <diablo22007@gmail.com>
2013-10-19 11:24:58 +0200

    Rotation now centers the image

--- src/rotation.ml
+++ src/rotation.ml
@@ -26,9 +26,9 @@ let dims_rot w h angle =
   let a = deg2rad angle in
   let hf = (float_of_int h) *. cos a +. (float_of_int w) *. sin a and
       wf = (float_of_int h) *. sin a +. (float_of_int w) *. cos a in
-  Printf.printf "h=%f w=%f" hf wf;
   ((int_of_float (ceil wf)), (int_of_float (ceil hf)))
 
+(* Apply the rotation to each pixel  of image *)
 let rotation image angle =
   let (w1, h1) = Image_tools.get_dim image in
   let (w, h) = dims_rot w1 h1 angle in 
@@ -38,9 +38,32 @@ let rotation image angle =
     for x = 0 to w1-1 do
       let pixel_color = Sdlvideo.get_pixel_color image x y in
       let (x_rot, y_rot) = rotate (x, y) (image_center w1 h1) angle in 
+      let (x_rot, y_rot) = (x_rot +. (float_of_int (w-w1))/.2.,
+                            y_rot +. (float_of_int (h-h1))/.2.) in (* Shift to recenter *)
       if (x_rot >= 0. && x_rot < (float_of_int w) && y_rot >= 0. && y_rot < (float_of_int h)) then
-	Sdlvideo.put_pixel_color new_surface (int_of_float x_rot) (int_of_float y_rot) pixel_color;
+	       Sdlvideo.put_pixel_color new_surface (int_of_float x_rot)
+                                              (int_of_float y_rot)
+                                              pixel_color;
     done;
   done;
-  Sdlvideo.save_BMP new_surface "kikou.bmp";
   new_surface
+
+(*
+
+  let detect_angle image = 
+    let (w, h) = get_dim image in
+    let detected_lines = Array.make_matrix 100 3 0 in
+    let i = 0 in
+    for y = 0 to h-1 do
+      for x = 0 to w-1 doux
+        let color = Sdlvideo.get_pixel_color image x y in
+        if color = (0,0,0) then
+          let theta = int_of_float (deg2rad (pi/2)) in
+            while theta < int_of_float (deg2rad (2*pi)) do
+              detected_lines.(i).(0) = detected_lines.(i).(0) + 1
+              detected_lines.(i).(1) = theta
+              detected_lines.(i).(2) = ro
+              incr i;
+            done;
+            for 
+          *)
