999312e7acd31242764a7b5d6c9fc4e4dd083bd9
Nicomplex <nico.pellissier.94@gmail.com>
2013-10-19 16:24:17 +0200

    convolution avec float

--- src/image_tools.ml
+++ src/image_tools.ml
@@ -27,28 +27,28 @@ let is_in_bound img x y =
 
 (*applique une matrice à un pixel de l'image*)
 let apply_matrix_px img_src img_dst x y matrix bias =
-  let r = ref 0 and g = ref 0 and b = ref 0
-  and posX = ref 0 and posY = ref 0 and factor = ref 1
+  let r = ref 0. and g = ref 0. and b = ref 0.
+  and posX = ref 0 and posY = ref 0 and factor = ref 1.
   and dimXmatrix = Array.length matrix and dimYmatrix = Array.length matrix.(0) in
   for i = 0 to dimXmatrix-1 do
     for j = 0 to dimYmatrix-1 do
-      factor := !factor + matrix.(i).(j);
+      factor := !factor +. matrix.(i).(j);
       posX := x - dimXmatrix / 2 + i;
       posY := y - dimYmatrix / 2 + j;
       if is_in_bound img_src !posX !posY then begin
         let (cr,cg,cb) = Sdlvideo.get_pixel_color img_src !posX !posY in
-        r := !r + cr * matrix.(i).(j);
-        g := !g + cg * matrix.(i).(j);
-        b := !b + cb * matrix.(i).(j); end
+        r := !r +. (float_of_int cr) *. matrix.(i).(j);
+        g := !g +. (float_of_int cg) *. matrix.(i).(j);
+        b := !b +. (float_of_int cb) *. matrix.(i).(j); end
     done; 
   done;
-  r := !r / !factor + bias;
-  g := !g / !factor + bias;
-  b := !b / !factor + bias;
-  if !r > 255 then r := 255 else if !r < 0 then r := 0;
-  if !g > 255 then g := 255 else if !g < 0 then g := 0;
-  if !b > 255 then b := 255 else if !b < 0 then b := 0;
-  Sdlvideo.put_pixel_color img_dst x y (!r,!g,!b);;
+  r := !r /. !factor +. bias;
+  g := !g /. !factor +. bias;
+  b := !b /. !factor +. bias;
+  if !r > 255. then r := 255. else if !r < 0. then r := 0.;
+  if !g > 255. then g := 255. else if !g < 0. then g := 0.;
+  if !b > 255. then b := 255. else if !b < 0. then b := 0.;
+  Sdlvideo.put_pixel_color img_dst x y (int_of_float !r,int_of_float !g,int_of_float !b);;
 
 (*applique une matrice de convolution a toute une image*)
 let apply_matrix src dst matrix bias =
